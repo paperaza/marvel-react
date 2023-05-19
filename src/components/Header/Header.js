52fdbae4edb9d74956e8a80406331c412c3ac284
paperaza <paperaza13@gimail.com>
2023-05-19 17:39:04 -0500

    Cambios en Header, Home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
 import './Header.scss';
+import {Menu} from 'semantic-ui-react';
 export default function Header() {
     return (
-        <div>
-            Estamos en Header
+        <div className="header-menu">
+            <Menu secondary>
+                <Menu.Item name="inicio"  onClick={()=>console.log("Haciendo clic en el menu")} />
+                <Menu.Item name="series"  onClick={()=>console.log("Haciendo clic en el series")} />
+                <Menu.Item name="comics"  onClick={()=>console.log("Haciendo clic en el comics")} />
+            </Menu>
         </div>
     );
     
